91531319653af01f19ccb1f9da3992440ecd23a7
Vinh Nguyen <vinhnguyen2308@gmail.com>
2021-10-30 17:23:49 +0700

    Initial Commit

---
+++ Sources/NVNetworkRequest/NVNetworkRequest.swift
@@ -0,0 +1,6 @@
+public struct NVNetworkRequest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/NVNetworkRequestTests/NVNetworkRequestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NVNetworkRequest
+
+final class NVNetworkRequestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NVNetworkRequest().text, "Hello, World!")
+    }
+}
